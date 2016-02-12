e9a7de9b489459859c35377e1b683bb89cb7ca6b
ueharajohji <ueharajohji@d2f2963a-3405-4d27-bcb1-dbf8b8db8f5a>
2016-02-12 15:56:37 +0000

    j.uehara 2016-02-1t3 0:56 tried animation for more modernized carousel. next is carousel contents and navbar design / sounds

--- js/custom/home.js
+++ js/custom/home.js
@@ -1,6 +1,43 @@
 $(document).ready(function (){
 
+var $carousel = $('#carouselMain');
+
+
+//initialize carousel
+
+$carousel.carousel();
+
+var $firstAnimatingElems =$carousel.find('.item:first').find('[data-animation ^= "animated"]');
+
+doAnimations($firstAnimatingElems);
+
+$carousel.carousel('pause');
+
+$carousel.on('slide.bs.carousel', function(){
+
+
+var $firstAnimatingElems =$carousel.find('.item:first').find('[data-animation ^= "animated"]');
+
+doAnimations($firstAnimatingElems);
 
 
 });
 
+});
+
+
+function doAnimations(elems){
+
+var animEndEv = 'webkitAnimationEnd animationend';
+
+elems.each(function(){
+var $this =$(this),
+    $animationType =$this.data('animation');
+
+    $this.addClass($animationType).one(animEndEv, function(){
+    $this.removeClass($animationType);
+    });
+
+});
+
+}
